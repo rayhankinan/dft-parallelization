16b4dfcee2802e57fa949a0e9731c95afe2658c3
rayhankinan <rayhankinan@gmail.com>
2023-02-22 13:13:45 +0700

    Coba coba open mpi

--- src/open-mpi/parallel.cpp
+++ src/open-mpi/parallel.cpp
@@ -18,10 +18,10 @@ class Matrix {
         vector<vector<double>> *data_ptr;
         vector<vector<double>> &data;
 
-        complex<double> handleElement(int k, int l, int m, int n) {
-            complex<double> sample = (k * m / (double) this->n) + (l * n / (double) this->n);
+        complex<double> handleElement(int k, int l, int i, int j) {
+            complex<double> sample = (k * i / (double) this->n) + (l * j / (double) this->n);
             complex<double> exponent = exp(-2.0i * pi() * sample);
-            complex<double> element = this->data[m][n] * exponent;
+            complex<double> element = this->data[i][j] * exponent;
 
             return element;
         }
@@ -29,18 +29,36 @@ class Matrix {
         complex<double> handlerRow(int k, int l, int i) {
             MPI_Init(NULL, NULL);
 
-            int world_size;
-            MPI_Comm_size(MPI_COMM_WORLD, &world_size);
-
             int world_rank;
             MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
 
             complex<double> row = 0;
-            for (int j = 0; j < this->n; j++) {
-                row += this->handleElement(k, l, i, j);
+            if (world_rank == 0) {
+                /* Master Process */
+
+                /* Send Index */
+                for (int j = 0; j < this->n; j++) {
+                    MPI_Send(&j, 1, MPI_INT, 1, 0, MPI_COMM_WORLD);
+                }
+
+                /* Receive Element */
+                complex<double> element;
+                for (int j = 0; j < this->n; j++) {
+                    MPI_Recv(&element, 1, MPI_DOUBLE_COMPLEX, 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+                    row += element;
+                }
+            } else {
+                /* Slave Process */
+
+                /* Receive Index */
+                int j;
+                MPI_Recv(&j, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
+                /* Send Element */
+                complex<double> element = this->handleElement(k, l, i, j);
+                MPI_Send(&element, 1, MPI_DOUBLE_COMPLEX, 0, 0, MPI_COMM_WORLD);
             }
 
-            MPI_Barrier(MPI_COMM_WORLD);
             MPI_Finalize();
 
             return row;
@@ -49,18 +67,36 @@ class Matrix {
         complex<double> handlerColumn(int k, int l) {
             MPI_Init(NULL, NULL);
 
-            int world_size;
-            MPI_Comm_size(MPI_COMM_WORLD, &world_size);
-
             int world_rank;
             MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
 
             complex<double> total = 0;
-            for (int i = 0; i < this->n; i++) {
-                total += this->handlerRow(k, l, i);
+            if (world_rank == 0) {
+                /* Master Process */
+
+                /* Send Index */
+                for (int i = 0; i < this->n; i++) {
+                    MPI_Send(&i, 1, MPI_INT, 1, 0, MPI_COMM_WORLD);
+                }
+
+                /* Receive Row */
+                complex<double> row;
+                for (int i = 0; i < this->n; i++) {
+                    MPI_Recv(&row, 1, MPI_DOUBLE_COMPLEX, 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+                    total += row;
+                }
+            } else {
+                /* Slave Process */
+
+                /* Receive Index */
+                int i;
+                MPI_Recv(&i, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
+                /* Send Row */
+                complex<double> row = this->handlerRow(k, l, i);
+                MPI_Send(&row, 1, MPI_DOUBLE_COMPLEX, 0, 0, MPI_COMM_WORLD);
             }
 
-            MPI_Barrier(MPI_COMM_WORLD);
             MPI_Finalize();
 
             return total;
@@ -88,15 +124,9 @@ class Matrix {
         }
 
         complex<double> dftElement(int k, int l) {
-            complex<double> element = 0;
-            for (int m = 0; m < this->n; m++) {
-                for (int n = 0; n < this->n; n++) {
-                    complex<double> sample = (k * m / (double) this->n) + (l * n / (double) this->n);
-                    complex<double> exponent = exp(-2.0i * pi() * sample);
-                    element += this->data[m][n] * exponent;
-                }
-            }
-            return element / (complex<double>)(this->n * this->n);
+            complex<double> total = this->handlerColumn(k, l);
+
+            return total / (complex<double>)(this->n * this->n);
         }
 };
 

a878513a4ab9e1b73d07b9412605f59f19940152
rayhankinan <rayhankinan@gmail.com>
2023-02-22 11:26:21 +0700

    Initialize open-mpi

---
+++ src/open-mpi/parallel.cpp
@@ -0,0 +1,116 @@
+// mpic++ parallel.cpp -o parallel
+
+#include <complex>
+#include <iostream>
+#include <vector>
+#include "mpi.h"
+
+using namespace std::complex_literals;
+using namespace std;
+
+constexpr complex<double> pi() {
+    return std::atan(1) * 4;
+}
+
+class Matrix {
+    private:
+        int n;
+        vector<vector<double>> *data_ptr;
+        vector<vector<double>> &data;
+
+        complex<double> handleElement(int k, int l, int m, int n) {
+            complex<double> sample = (k * m / (double) this->n) + (l * n / (double) this->n);
+            complex<double> exponent = exp(-2.0i * pi() * sample);
+            complex<double> element = this->data[m][n] * exponent;
+
+            return element;
+        }
+
+        complex<double> handlerRow(int k, int l, int i) {
+            MPI_Init(NULL, NULL);
+
+            int world_size;
+            MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+
+            int world_rank;
+            MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+
+            complex<double> row = 0;
+            for (int j = 0; j < this->n; j++) {
+                row += this->handleElement(k, l, i, j);
+            }
+
+            MPI_Barrier(MPI_COMM_WORLD);
+            MPI_Finalize();
+
+            return row;
+        }
+
+        complex<double> handlerColumn(int k, int l) {
+            MPI_Init(NULL, NULL);
+
+            int world_size;
+            MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+
+            int world_rank;
+            MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+
+            complex<double> total = 0;
+            for (int i = 0; i < this->n; i++) {
+                total += this->handlerRow(k, l, i);
+            }
+
+            MPI_Barrier(MPI_COMM_WORLD);
+            MPI_Finalize();
+
+            return total;
+        }
+
+    public:
+        Matrix() : data_ptr(new vector<vector<double>>()), data(*data_ptr) {
+            this->data.resize(10, vector<double>(10, 0));
+        }
+
+        ~Matrix() {
+            delete data_ptr;
+        }
+
+        void readMatrix() {
+            cin >> this->n;
+            this->data.resize(this->n, vector<double>(this->n, 0.0));
+            for (int i = 0; i < this->n; i++)
+                for (int j = 0; j < this->n; j++)
+                    cin >> this->data[i][j];
+        }
+
+        int size() {
+            return this->n;
+        }
+
+        complex<double> dftElement(int k, int l) {
+            complex<double> element = 0;
+            for (int m = 0; m < this->n; m++) {
+                for (int n = 0; n < this->n; n++) {
+                    complex<double> sample = (k * m / (double) this->n) + (l * n / (double) this->n);
+                    complex<double> exponent = exp(-2.0i * pi() * sample);
+                    element += this->data[m][n] * exponent;
+                }
+            }
+            return element / (complex<double>)(this->n * this->n);
+        }
+};
+
+int main(void) {
+    Matrix *source = new Matrix();
+    source->readMatrix();
+
+    complex<double> sum = 0.0;
+    for (int i = 0; i < source->size(); i++) {
+        for (int j = 0; j < source->size(); j++) {
+            complex<double> el = source->dftElement(i, j);
+            sum += el;
+        }
+    }
+    cout << sum / (double) source->size();
+    return 0;
+}
